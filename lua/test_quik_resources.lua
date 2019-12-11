20fbb7b4aff2fdcce6b5ead1de729cc51fd2c006
toxa <untoxa@mail.ru>
2019-12-11 18:07:45 +0300

    enhanced test script

--- lua/test_quik_resources.lua
+++ lua/test_quik_resources.lua
@@ -1,44 +1,45 @@
 package.cpath = getScriptPath() .. "/?.dll"
 qres = require "lua_quik_resources"
 
-function main()  
-  quik_resources_lib = "lang_rus.dll"
+quik_resources_lib = "lang_res.dll" -- "lang_rus.dll" for QUIK8!
 
-  hQUIK = qres.get_quik_handle()
-  if (hQUIK ~= 0) then
-    message("QUIK main window handle: " .. tostring(hQUIK))
+qres.HWND_DESKTOP = 0
+qres.WM_COMMAND = 273
+qres.INFOMENU_CONNECT = 100
+qres.INFOMENU_DISCONNECT = 101
+qres.IDOK = 1
+qres.MF_ENABLED = 0
 
-    conn_dlg_title = tostring(qres.get_dlg_title(10107))
-    message("Connect dialog caption: " .. conn_dlg_title, 1)
+QUIK_Handle = qres.get_quik_handle()
+QUIK_conn_dlg_title = qres.get_dlg_title(10107); if QUIK_conn_dlg_title == nil then QUIK_conn_dlg_title = ""; end
 
-    HWND_DESKTOP = 0
-    WM_COMMAND = 273
-    INFOMENU_CONNECT = 100
-    INFOMENU_DISCONNECT = 101
-    IDOK = 1
-    MF_ENABLED = 0
-
-    connect_menu_state = qres.get_menu_state(hQUIK, INFOMENU_CONNECT)
-    message("Connect menu item state: " .. tostring(connect_menu_state))
-
-    if (connect_menu_state == MF_ENABLED) then
-      qres.post_message(hQUIK, WM_COMMAND, INFOMENU_CONNECT, 0)
-      sleep(100)
-
-      hConnDlg = qres.get_child_handle(HWND_DESKTOP, conn_dlg_title)
-      if (hConnDlg ~= 0) then
-        message("Connection dialog handle: " .. tostring(hConnDlg))
-
-        qres.set_dlg_item_text(hConnDlg, 10101, "quik_login")
-        qres.set_dlg_item_text(hConnDlg, 10102, "quik_password")
-        qres.post_message(hConnDlg, WM_COMMAND, IDOK, 0)
-      else
-        message("Unable to get connection dialog handle", 2)
-      end
+function manage_quik_connection(connect, quik_login, quik_password)  
+    if connect then 
+        if (qres.get_menu_state(QUIK_Handle, qres.INFOMENU_CONNECT) == qres.MF_ENABLED) then
+            qres.post_message(QUIK_Handle, qres.WM_COMMAND, qres.INFOMENU_CONNECT, 0)
+            sleep(100)
+            local hConnDlg = qres.get_child_handle(qres.HWND_DESKTOP, QUIK_conn_dlg_title)
+            if (hConnDlg ~= 0) then
+                qres.set_dlg_item_text(hConnDlg, 10101, quik_login)
+                qres.set_dlg_item_text(hConnDlg, 10102, quik_password)
+                qres.post_message(hConnDlg, qres.WM_COMMAND, qres.IDOK, 0)
+                return "Login sent"
+            end
+        else
+            return "Already connected or connecting"
+        end
     else
-      message("Connect menu item is grayed or disabled", 2)
+        qres.post_message(QUIK_Handle, qres.WM_COMMAND, qres.INFOMENU_DISCONNECT, 0)
+        return "Logout sent"
     end
-  else
-    message("Unable to get QUIK main window handle", 2)
-  end
+    return "Internal error"
+end
+ 
+function main()
+    if (QUIK_Handle ~= 0) then
+       local result = manage_quik_connection(true, "login", "password")
+       if result ~= nil then message(result, 1); end
+    else
+      message("Unable to find QUIK main window handle", 1)
+    end
 end
